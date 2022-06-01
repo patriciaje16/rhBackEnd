2a4a8c3c1b5e2f104f00186550620e5a55f2cf35
Patricia Edwards <patriciaje16@gmail.com>
2022-06-01 12:18:51 +0100

    first commit

---
+++ src/main/java/com/example/housewives/housewives/HousewivesApplication.java
@@ -0,0 +1,13 @@
+package com.example.housewives.housewives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousewivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousewivesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/housewives/housewives/HousewivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housewives.housewives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousewivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
